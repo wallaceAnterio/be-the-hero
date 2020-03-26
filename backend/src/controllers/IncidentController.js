1a888993f3e67069c4628cb693ce0c9536778672
wallaceAnterio <wallacesanterio@gmail.com>
2020-03-26 13:31:13 -0300

    Front-end do App

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -11,12 +11,12 @@ module.exports = {
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select([
+            .select([                
                 'incidents.*',
                 'ongs.name', 
                 'ongs.email', 
                 'ongs.whatsapp',
-                'ong.city',
+                'ongs.city',
                 'ongs.uf'
 
             ]);
